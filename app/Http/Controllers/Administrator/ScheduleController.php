26afa90b8823643f6493cc0325bd0be4782a20be
akashproject <akashdutta.scriptcrown@gmail.com>
2024-05-21 18:39:55 +0530

    update 2023

--- app/Http/Controllers/Administrator/ScheduleController.php
+++ app/Http/Controllers/Administrator/ScheduleController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Administrator;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
     //
+    
 }
