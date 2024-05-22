4b6e5564bca4b0ff4bedfc5de9f6c4b8b3df0892
akashproject <akashdutta.scriptcrown@gmail.com>
2024-05-22 19:20:41 +0530

    update 2023

--- resources/views/administrator/users/slots.blade.php
+++ resources/views/administrator/users/slots.blade.php
@@ -14,20 +14,20 @@
         <div class="alert alert-success" role="alert">{{ session()->get('message') }}</div>
     @endif
 	</div>
-	
+
 	@if($schedules)
-		<div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
+		<div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
 			@foreach($schedules as $value)
-			<div class="col">
-				<div class="card shadow-none bg-transparent border border-success mb-3">
+			<div class="col-md-3">
+				<label for="slots_{{$value->id}}" class="card shadow-none bg-white mb-2">
+					<input type="checkbox" id="slots_{{$value->id}}" name="slots[]" value="{{ $value->id }}" style="visibility: hidden;">	
 					<div class="card-body">
 						<div class="row" >
 							<h5 class="card-title  mb-0">{{ $value->day }}  </h5>
 							<p class="card-text">{{ $value->start_time }} - {{ $value->end_time }}</p>
 						</div>
-							
 					</div>
-				</div>
+				</label>
 			</div>
 			@endforeach
 		</div>
